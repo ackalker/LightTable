3db4a0ee0d90981db928628f8bb7240c25772e89
Alain Kalker <a.c.kalker@gmail.com>
2014-03-12 16:04:06 +0100

    Rework linux_deps.sh

--- linux_deps.sh
+++ linux_deps.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 # Check if lein is installed
 lein version >/dev/null 2>&1 || { echo >&2 "Please install leiningen before running this script."; exit 1; }
-if [ "$(echo `lein version` | grep 'Leiningen 1.\|2.0')" ]; then 
+if [ "$(echo `lein version` | grep 'Leiningen \(1.\|2.0\)')" ]; then 
 	echo "lein version must be 2.1 or above. Do a lein upgrade first"; exit 1;
 fi
+
+# Check if node is installed
+node --version >/dev/null 2>&1 || { echo >&2 "Please install NodeJS before running this script."; exit 1; }
+if ! [ "$(echo `node --version` | grep 'v0.1[012]')" ]; then 
+	echo "node version must be 0.10 or above. Update NodeJS first"; exit 1;
+fi
+
+mkdir -p deploy/plugins/node
+ln -sf $(which node) deploy/plugins/node/node
 
 which curl &> /dev/null
 if [ $? -ne 0 ]; then
@@ -11,25 +20,18 @@ if [ $? -ne 0 ]; then
 	exit
 fi
 
-echo "### Fetching binaries ###"
-BITS=""
-if [ $(getconf LONG_BIT) == "64" ]; then BITS="64"; fi
-TARBALL=LightTableLinux$BITS.tar.gz
-curl -O http://d35ac8ww5dfjyg.cloudfront.net/playground/bins/0.6.0/$TARBALL
+echo "### Fetching node-webkit ###"
+ARCH=""
+if [ $(getconf LONG_BIT) == "64" ]; then ARCH="x64"; else ARCH="ia32"; fi
+TARBALL=node-webkit-v0.8.5-linux-$ARCH.tar.gz
+curl -O https://s3.amazonaws.com/node-webkit/v0.8.5/$TARBALL
 tar -xzf $TARBALL
 rm $TARBALL
-cp -ar deploy/* LightTable
-rm -rf deploy
-mv LightTable deploy
-
-echo "### Building cljs ###"
-lein cljsbuild clean && lein cljsbuild once
+cp -ar ${TARBALL%.tar.gz}/* deploy
+rm -rf ${TARBALL%.tar.gz}
 
 echo "### Fetching clojure plugin ###"
+mkdir -p deploy/plugins
 cd deploy/plugins
 rm -rf clojure
 git clone https://github.com/LightTable/Clojure.git clojure
-
-echo "### Building clojure plugin ###"
-cd clojure
-./build.sh
