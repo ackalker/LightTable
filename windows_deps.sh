a37a58f80ddb9ac843d70bae2cd315a9f13f4fe8
Alain Kalker <a.c.kalker@gmail.com>
2014-03-13 13:50:16 +0100

    Rework windows_deps.sh

--- windows_deps.sh
+++ windows_deps.sh
@@ -1,27 +1,32 @@
 # Check if lein is installed
 lein version >/dev/null 2>&1 || { echo >&2 "Please install leiningen before running this script."; exit 1; }
-if [ "$(echo `lein version` | grep 'Leiningen 1.\|2.0')" ]; then 
+if [ "$(echo `lein version` | grep 'Leiningen \(1.\|2.0\)')" ]; then 
 	echo "lein version must be 2.1 or above. Do a lein upgrade first"; exit 1;
 fi
 
-ZIPFILE=LightTableWin.zip
-echo "### Fetching binaries ###"
-curl -O http://d35ac8ww5dfjyg.cloudfront.net/playground/bins/0.6.0/$ZIPFILE
-unzip $ZIPFILE
+# Check if node is installed
+node --version >/dev/null 2>&1 || { echo >&2 "Please install NodeJS before running this script."; exit 1; }
+if ! [ "$(echo `node --version` | grep 'v0.1[012]')" ]; then 
+	echo "node version must be 0.10 or above. Update NodeJS first"; exit 1;
+fi
+
+mkdir -p deploy/plugins/node
+cp `cygpath --unix $(node -e 'console.log(process.execPath)')` deploy/plugins/node/node.exe
+
+# Check if curl is installed
+curl --version >/dev/null 2>&1 || { echo >&2 "Please install curl before running this script."; exit 1; }
+
+echo "### Fetching node-webkit ###"
+ZIPFILE=node-webkit-v0.8.5-win-ia32.zip
+curl -O https://s3.amazonaws.com/node-webkit/v0.8.5/$ZIPFILE
+cd deploy
+unzip ../$ZIPFILE
+cd ..
 rm $ZIPFILE
-cp -ar deploy/* LightTable
-rm -rf deploy
-mv LightTable deploy
 chmod u+rwx deploy
 
-echo "### Building cljs ###"
-lein cljsbuild clean && lein cljsbuild once
-
 echo "### Fetching clojure plugin ###"
+mkdir -p deploy/plugins
 cd deploy/plugins
 rm -rf clojure
 git clone https://github.com/LightTable/Clojure.git clojure
-
-echo "### Building clojure plugin ###"
-cd clojure
-./build.sh
