efee7e871ce94d3e2dc0f7be247916dbde90db57
Alain Kalker <a.c.kalker@gmail.com>
2014-03-13 23:39:34 +0100

    Rework osx_deps.sh

--- osx_deps.sh
+++ osx_deps.sh
@@ -4,35 +4,17 @@ if [ "$(echo `lein version` | grep 'Leiningen \(1.\|2.0\)')" ]; then
 	echo "lein version must be 2.1 or above. Do a lein upgrade first"; exit 1;
 fi
 
-
-#remove the previously built plugins, binary, and executable
-rm -rf deploy/plugins
-rm -rf deploy/LightTable.app
-rm deploy/light
-
-#get the LightTable.app binary
-curl -O http://d35ac8ww5dfjyg.cloudfront.net/playground/bins/0.6.2/LightTableMac.zip
-unzip LightTableMac.zip
-mv LightTable/* deploy/
-rmdir LightTable/
-rm LightTableMac.zip
-
-cp -r platform/mac/* deploy/
-
-#build the core cljs of LightTable
-lein cljsbuild clean && lein cljsbuild once
-
-#Get the plugins from the binary
+echo "### Fetching node-webkit ###"
+ZIPFILE=node-webkit-v0.8.5-osx-ia32.zip
+curl -O https://s3.amazonaws.com/node-webkit/v0.8.5/$ZIPFILE
 cd deploy
-mv LightTable.app/Contents/Resources/app.nw/plugins plugins
-cd plugins
+unzip ../$ZIPFILE
+cd ..
+rm $ZIPFILE
+chmod u+rwx deploy
 
-#Make sure we have the latest clojure
+echo "### Fetching clojure plugin ###"
+mkdir -p deploy/plugins
+cd deploy/plugins
 rm -rf clojure
 git clone https://github.com/LightTable/Clojure.git clojure
-cd clojure
-./build.sh
-
-#Remove the app.nw so we use our local copy
-cd ../../
-rm -rf LightTable.app/Contents/Resources/app.nw
